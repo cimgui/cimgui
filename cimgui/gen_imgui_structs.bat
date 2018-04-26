c735efe55a9b29ab7f6ad3bb1137e62f37a6bbc0
sonoro1234 <sonoro@telefonica.net>
2018-04-26 19:51:47 +0200

    automatically generate imgui_structs.h from imgui.h with lua

---
+++ cimgui/gen_imgui_structs.bat
@@ -0,0 +1,8 @@
+rem this is used to rebuild imgui_structs.h
+rem set your PATH if necessary for gcc and lua with:
+set PATH=%PATH%;C:\mingw32\bin;C:\luaGL;
+
+gcc -E   ../imgui/imgui.h | luajit.exe ./gen_imgui_structs.lua > imgui_structs.h
+
+cmd /k
+
