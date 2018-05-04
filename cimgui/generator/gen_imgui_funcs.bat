a6fdb24ff58e93a06f5b9a2fea86940c9dfd2797
sonoro1234 <sonoro@telefonica.net>
2018-05-04 12:00:01 +0200

    generator in folder

--- cimgui/generator/gen_imgui_funcs.bat
+++ cimgui/generator/gen_imgui_funcs.bat
@@ -2,7 +2,8 @@ rem this is used to rebuild imgui_structs.h
 rem set your PATH if necessary for gcc and lua with:
 set PATH=%PATH%;C:\mingw32\bin;C:\luaGL;
 
-gcc -E   ../imgui/imgui.h | luajit.exe ./gen_imgui_structs.lua > imgui_structs.h
-rem gcc -E   ../imgui/imgui.h > imgui_structs.raw
+gcc -E  -DIMGUI_DISABLE_OBSOLETE_FUNCTIONS ../../imgui/imgui.h | luajit.exe ./gen_imgui_funcs.lua > auto_funcs.txt
+
 cmd /k
+
 
--- cimgui/generator/gen_imgui_structs.bat
+++ cimgui/generator/gen_imgui_structs.bat
@@ -2,8 +2,7 @@ rem this is used to rebuild imgui_structs.h
 rem set your PATH if necessary for gcc and lua with:
 set PATH=%PATH%;C:\mingw32\bin;C:\luaGL;
 
-gcc -E  -DIMGUI_DISABLE_OBSOLETE_FUNCTIONS ../imgui/imgui.h | luajit.exe ./gen_imgui_funcs.lua > auto_funcs.txt
-
+gcc -E   ../../imgui/imgui.h | luajit.exe ./gen_imgui_structs.lua > imgui_structs.h
+rem gcc -E   ../imgui/imgui.h > imgui_structs.raw
 cmd /k
-
 
