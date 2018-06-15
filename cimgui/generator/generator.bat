017b935afd9bcc69ef5aa71432ab3d01a626d5e5
sonoro1234 <sonoro@telefonica.net>
2018-06-15 14:24:25 +0200

    gen with comments

--- cimgui/generator/generator.bat
+++ cimgui/generator/generator.bat
@@ -3,7 +3,11 @@ rem set your PATH if necessary for gcc and lua with:
 set PATH=%PATH%;C:\mingw32\bin;C:\luaGL;
 
 rem gcc -E -C -DIMGUI_DISABLE_OBSOLETE_FUNCTIONS  ../../imgui/imgui.h | luajit.exe ./generator.lua > out.txt
-gcc -E -DIMGUI_DISABLE_OBSOLETE_FUNCTIONS  ../../imgui/imgui.h | luajit.exe ./generator.lua > out.txt
+rem gcc -E -DIMGUI_DISABLE_OBSOLETE_FUNCTIONS  ../../imgui/imgui.h | luajit.exe ./generator.lua > out.txt
 rem gcc -E -CC -DIMGUI_DISABLE_OBSOLETE_FUNCTIONS  ../../imgui/imgui.h > imgui_structs2.raw
+rem gcc -E -DIMGUI_DISABLE_OBSOLETE_FUNCTIONS  ../../imgui/imgui.h > 1.txt
+rem gcc -E -CC -DIMGUI_DISABLE_OBSOLETE_FUNCTIONS  ../../imgui/imgui.h > 1CC.txt
+rem gcc -E -P -DIMGUI_DISABLE_OBSOLETE_FUNCTIONS  ../../imgui/imgui.h > 1P.txt
+type 1.txt | luajit.exe ./generator.lua > out.txt
 cmd /k
 
