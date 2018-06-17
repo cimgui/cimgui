41e975cf55f0a9672fb222b16c0fbe6306011196
sonoro1234 <sonoro@telefonica.net>
2018-06-17 12:26:40 +0200

    having GCC and NOGCC in same script

--- cimgui/generator/generator.bat
+++ cimgui/generator/generator.bat
@@ -5,10 +5,16 @@ set PATH=%PATH%;C:\mingw32\bin;C:\luaGL;
 rem gcc -E -C -DIMGUI_DISABLE_OBSOLETE_FUNCTIONS  ../../imgui/imgui.h | luajit.exe ./generator.lua > out.txt
 rem gcc -E -DIMGUI_DISABLE_OBSOLETE_FUNCTIONS  ../../imgui/imgui.h | luajit.exe ./generator.lua > out.txt
 rem gcc -E -CC -DIMGUI_DISABLE_OBSOLETE_FUNCTIONS  ../../imgui/imgui.h > imgui_structs2.raw
-rem gcc -E -DIMGUI_DISABLE_OBSOLETE_FUNCTIONS  ../../imgui/imgui.h > 1.txt
+
+gcc -E -DIMGUI_DISABLE_OBSOLETE_FUNCTIONS  ../../imgui/imgui.h > 1.txt
+type 1.txt | luajit.exe ./generator.lua true > out.txt
+
 rem gcc -E -C -traditional-cpp -DIMGUI_DISABLE_OBSOLETE_FUNCTIONS  ../../imgui/imgui.h > 1Ctra.txt
+rem type 1CTra.txt | luajit.exe ./generator.lua true > out.txt
+
 rem gcc -E -P -DIMGUI_DISABLE_OBSOLETE_FUNCTIONS  ../../imgui/imgui.h > 1P.txt
 rem type 1C.txt | luajit.exe ./generator.lua > out.txt
-type 1CTra.txt | luajit.exe ./generator.lua > out.txt
+
+
 cmd /k
 
--- cimgui/generator/generatorNOGCC.bat
+++ cimgui/generator/generatorNOGCC.bat
@@ -2,7 +2,7 @@ rem this is used to rebuild imgui_structs.h
 rem set your PATH if necessary for gcc and lua with:
 set PATH=%PATH%;C:\luaGL;
 
-type ..\..\imgui\imgui.h  | luajit.exe ./generatorNOGCC.lua > out.txt
+type ..\..\imgui\imgui.h  | luajit.exe ./generator.lua false > out.txt
 
 cmd /k
 
