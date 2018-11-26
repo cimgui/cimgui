df00156746f39fdb14b2ca3c336bc882a6f7d95a
sonoro1234 <sonoro@telefonica.net>
2018-11-26 17:54:36 +0100

    output json files with newline

--- generator/json.lua
+++ generator/json.lua
@@ -82,7 +82,7 @@ local function encode_table(val, stack)
       table.insert(res, encode(v, stack))
     end
     stack[val] = nil
-    return "[" .. table.concat(res, ",") .. "]"
+    return "[" .. table.concat(res, ",\n") .. "]"
 
   else
     -- Treat as an object
@@ -93,7 +93,7 @@ local function encode_table(val, stack)
       table.insert(res, encode(k, stack) .. ":" .. encode(v, stack))
     end
     stack[val] = nil
-    return "{" .. table.concat(res, ",") .. "}"
+    return "{" .. table.concat(res, ",\n") .. "}"
   end
 end
 
