8c817de19503b02edbb6a66c9b187206126ca9a5
aodq <dtoadq@gmail.com>
2019-07-30 23:15:39 -0400

    adds generator shell script

---
+++ generator/generator.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/sh
+
+# this script must be executed in this directory
+# all the output goes to generator/output folder
+# .cpp and .h files:
+# cimgui.h and cimgui.cpp with gcc preprocess
+# cimgui_nopreprocess.h and cimgui_nopreprocess.cpp generated without preprocess
+# cimgui_impl.h with implementation function cdefs
+# lua and json files:
+# definitions.lua for function definitions
+# structs_and_enums.lua with struct and enum information-definitions
+# impl_definitions.lua for implementation function definitions
+
+#process  files
+# arg[1] compiler name gcc, clang, cl or nocompiler
+# arg[2..n] name of implementations to generate
+luajit ./generator.lua gcc glfw opengl3 opengl2 sdl
