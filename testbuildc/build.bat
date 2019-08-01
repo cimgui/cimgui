14e8be9af1d422b2d47851f415625be068e337f0
Leonardo Mariscal <leo@ldmd.mx>
2019-07-31 19:30:36 -0500

    Update CMake to a more modern module management system and create simple test based on imgui null example

--- testbuildc/build.bat
+++
@@ -1,6 +0,0 @@
-:: set PATH=%PATH%;C:\mingw32\bin;
-set PATH=%PATH%;C:\i686-7.2.0-release-posix-dwarf-rt_v5-rev1\mingw32\bin;
-::gcc -std=c99 -DCIMGUI_DEFINE_ENUMS_AND_STRUCTS cimgui.h
-::gcc  -DCIMGUI_DEFINE_ENUMS_AND_STRUCTS cimgui.h
-gcc  -std=c99 -Wall -Wpedantic -DCIMGUI_DEFINE_ENUMS_AND_STRUCTS cimguitest.c
-cmd /k
