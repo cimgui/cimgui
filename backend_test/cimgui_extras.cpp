873c03c3673033bf8e8dd22901d4a3934b7407b2
Victor Bombi <sonoroster@gmail.com>
2021-08-22 13:48:02 +0200

    backend_test: use custom imgui gl3w

--- backend_test/cimgui_extras.cpp
+++ backend_test/cimgui_extras.cpp
@@ -1,11 +1,10 @@
 #include "../imgui/imgui.h"
 
-// GL3W/GLFW
-#include <GL/gl3w.h>    // This example is using gl3w to access OpenGL functions (because it is small). You may use glew/glad/glLoadGen/etc. whatever already works for you.
+#include "../imgui/backends/imgui_impl_opengl3_loader.h"
 
 //making it accesible for luajit
 #ifdef _WIN32
-extern "C" __declspec( dllexport ) int Do_gl3wInit(void){ return gl3wInit();};
+extern "C" __declspec( dllexport ) int Do_gl3wInit(void){ return imgl3wInit();};
 #else
-extern "C"	int Do_gl3wInit(void){ return gl3wInit();};
+extern "C"	int Do_gl3wInit(void){ return imgl3wInit();};
 #endif
