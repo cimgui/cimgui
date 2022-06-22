44b388121a32088a88a442bd807bc2e930520a6c
sonoro1234 <sonoroster@gmail.com>
2022-06-22 12:43:41 +0200

    generator: dont error if VULKAN_SDK==nil (because it is executed in no vulkan generation also)

--- generator/config_generator.lua
+++ generator/config_generator.lua
@@ -1,3 +1,3 @@
 return {
-	vulkan = {(os.getenv("VULKAN_SDK") or error"vulkan SDL not found").."/Include"}, --{[[C:\VulkanSDK\1.3.216.0\Include]]}
+	vulkan = {(os.getenv("VULKAN_SDK") or "vulkan_SDK_not_found").."/Include"}, --{[[C:\VulkanSDK\1.3.216.0\Include]]}
 } 
