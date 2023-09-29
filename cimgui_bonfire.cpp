1b5c3a850c01a273c026e93289d69f928ef529e0
Sam Christiansen <sam@bonfirestudios.com>
2023-09-29 14:25:08 -0700

    adding some more methods

--- cimgui_bonfire.cpp
+++ cimgui_bonfire.cpp
@@ -21,6 +21,11 @@ CIMGUI_API void igSetNextWindowSizeXY(float sizeX, float sizeY, ImGuiCond cond)
     return ImGui::SetNextWindowSize(ImVec2(sizeX, sizeY),cond);
 }
 
+CIMGUI_API void igSetWindowSizeXY(float sizeX, float sizeY, ImGuiCond cond)
+{
+    return ImGui::SetWindowSize(ImVec2(sizeX, sizeY),cond);
+}
+
 CIMGUI_API void igSetNextWindowPosXY(float posX, float posY, ImGuiCond cond, float pivotX, float pivotY)
 {
     return ImGui::SetNextWindowPos(ImVec2(posX, posY),cond,ImVec2(pivotX, pivotY));
@@ -32,4 +37,19 @@ CIMGUI_API void igTextColoredRGBA(float r, float g, float b, float a,const char*
     va_start(args, fmt);
     ImGui::TextColoredV(ImVec4(r, g, b, a),fmt,args);
     va_end(args);
+}
+
+CIMGUI_API bool igInputTextBurst(const char* label, char* text, size_t capacity, ImGuiInputTextFlags flags)
+{
+    return ImGui::InputText(label, text, capacity, flags, 0x0, 0x0);
+}
+
+CIMGUI_API bool igSelectable_BoolXY(const char* label, bool selected, ImGuiSelectableFlags flags, float x, float y)
+{
+    return igSelectable_Bool(label, selected, flags, ImVec2(x, y));
+}
+
+CIMGUI_API bool igBeginChild_StrXY(const char* label, float x, float y, bool border, ImGuiSelectableFlags flags)
+{
+    return igBeginChild_Str(label, ImVec2(x, y), border, flags);
 }
